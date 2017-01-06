76b174f610036a147a67fd00e010585931066829
alegrandunice <ag.legrand@sfr.fr>
2017-01-06 11:33:33 +0100

    PAA-131 Intégration des Frameworks Karma et Jasmine dans le projet (test unitaire)

---
+++ app/test.spec.ts
@@ -0,0 +1,7 @@
+
+
+describe("Hello world", function() {
+    it("says hello", function() {
+        expect("Hello world!").toEqual("Hello world!");
+    });
+});
--- systemjs.config.js
+++ systemjs.config.js
@@ -1,9 +1,3 @@
-/**
- * System configuration for Angular samples
- * Adjust as necessary for your application needs.
- */
-declare var System: any;
-
 System.config({
     paths: {
         // paths serve as alias
@@ -13,7 +7,6 @@ System.config({
     map: {
         // our app is within the app folder
         'app': 'app',
-
         // angular bundles
         '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
         '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -24,7 +17,6 @@ System.config({
         '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
         '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
         '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
-
         // other libraries
         'rxjs': 'npm:rxjs',
         'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
@@ -38,6 +30,5 @@ System.config({
         'rxjs': {
             defaultExtension: 'js'
         },
-
     }
 });
