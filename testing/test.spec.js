96a4d5b22962d83d9d89352c858f36167c9a70e8
alegrandunice <ag.legrand@sfr.fr>
2017-01-07 20:08:24 +0100

    Testing/ directory : Delete .js and .map

--- testing/test.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Hello world", function () {
-    it("says hello", function () {
-        expect("Hello world!").toEqual("Hello world!");
-    });
-});
-//# sourceMappingURL=test.spec.js.map
