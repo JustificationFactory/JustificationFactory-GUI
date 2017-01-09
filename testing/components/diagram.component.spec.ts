96a1e5c7677b25aea16128204fa66a74a325253b
alegrandunice <ag.legrand@sfr.fr>
2017-01-09 11:28:13 +0100

    PAA-140 Unit tests: components/diagram.component.ts

--- testing/components/diagram.component.spec.ts
+++ testing/components/diagram.component.spec.ts
@@ -7,50 +7,51 @@ import {PaletteComponent} from "../../app/components/palette.component";
 
 describe("diagram.component", () => {
 
+    let comp:    DiagramComponent;
+    let fixture: ComponentFixture<DiagramComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ DiagramComponent, PropertiesComponent, ActionsToolbarComponent, EditToolbarComponent, PaletteComponent ], // declare the test component
+        });
+
+        // Overrides here, if you need them
+        TestBed.overrideComponent(DiagramComponent, {
+            set: {
+                templateUrl: 'base/app/components/diagram.component.html'
+            }
+        })
+        TestBed.overrideComponent(PropertiesComponent, {
+            set: {
+                templateUrl: 'base/app/components/properties.component.html'
+            }
+        });
+        TestBed.overrideComponent(ActionsToolbarComponent, {
+            set: {
+                templateUrl: 'base/app/components/actions.toolbar.component.html'
+            }
+        });
+        TestBed.overrideComponent(EditToolbarComponent, {
+            set: {
+                templateUrl: 'base/app/components/edit.toolbar.component.html'
+            }
+        });
+        TestBed.overrideComponent(PaletteComponent, {
+            set: {
+                templateUrl: 'base/app/components/palette.component.html'
+            }
+        });
+
+        TestBed.compileComponents().then(() => {
+            //compilation succeed (async needed)
+        }).catch(function(e) {
+            console.log(e); // "zut !"
+        });
+
+    }));
+
+
     describe("Initial values", () => {
-        let comp:    DiagramComponent;
-        let fixture: ComponentFixture<DiagramComponent>;
-
-        beforeEach(async(() => {
-            TestBed.configureTestingModule({
-                declarations: [ DiagramComponent, PropertiesComponent, ActionsToolbarComponent, EditToolbarComponent, PaletteComponent ], // declare the test component
-            });
-
-            // Overrides here, if you need them
-            TestBed.overrideComponent(DiagramComponent, {
-                set: {
-                    templateUrl: 'base/app/components/diagram.component.html'
-                }
-            })
-            TestBed.overrideComponent(PropertiesComponent, {
-                set: {
-                    templateUrl: 'base/app/components/properties.component.html'
-                }
-            });
-            TestBed.overrideComponent(ActionsToolbarComponent, {
-                set: {
-                    templateUrl: 'base/app/components/actions.toolbar.component.html'
-                }
-            });
-            TestBed.overrideComponent(EditToolbarComponent, {
-                set: {
-                    templateUrl: 'base/app/components/edit.toolbar.component.html'
-                }
-            });
-            TestBed.overrideComponent(PaletteComponent, {
-                set: {
-                    templateUrl: 'base/app/components/palette.component.html'
-                }
-            });
-
-            TestBed.compileComponents().then(() => {
-                //compilation succeed (async needed)
-            }).catch(function(e) {
-                console.log(e); // "zut !"
-            });
-
-        }));
-
         it('Graph Scale = 1', () => {
             fixture = TestBed.createComponent(DiagramComponent);
             comp = fixture.componentInstance; // DiagramComponent test instance
@@ -66,15 +67,31 @@ describe("diagram.component", () => {
             fixture.detectChanges();
             expect(comp.selectedElement).toBeNull();
         });
+
     });
 
-    //Le chargement d'un diagramme fonctionne (myholder + nombre d'éléments)
+    describe("Loading diagram", () => {
 
-    //Charque élément à un parent de type Cell
+        it('myholder filled and number of elements OK', () => {
+            expect(true).toEqual(true);
+        });
 
-    //La sélection d'un élément fonctionne (deux sélections à la suite)
+    });
 
-    //Le zoom fonctionne correctement
+    describe("Diagram manipulations", () => {
 
-    //Le déplacement du diagramme fonctionne correctement (sur clique gauche de la souris)
+        it('We can select an element two times and have correct property label each time', () => {
+            expect(true).toEqual(true);
+        });
+
+        it('Zoom method change diagram scale', () => {
+            expect(true).toEqual(true);
+        });
+
+        it('The diagram move correctly when we mode the cursor with left click down', () => {
+            expect(true).toEqual(true);
+        });
+
+    });
+
 });
