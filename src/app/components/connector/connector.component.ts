87dbe0d88a98da45ddf2a188892de869213f25ca
Mathias Chevalier <mc.mumrau@gmail.com>
2017-11-29 16:37:58 +0100

    [DEV] Oups commit oublié

--- src/app/components/connector/connector.component.ts
+++ src/app/components/connector/connector.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {WsConnectorService} from '../../services/webServices/ws-connector.service';
-import {ParseJson2DiagramElements} from '../../business/diagram/importDiagram';
+import {ParseDiagramElementsResult, ParseJson2DiagramElements} from '../../business/diagram/importDiagram';
+import {DiagramComponent} from "../diagram/diagram.component";
 
 @Component({
   selector: 'app-connector',
@@ -12,15 +13,17 @@ export class ConnectorComponent implements OnInit {
   public argSystemIdList: string[];
   public currentArgSystem: ArgSystem;
 
-  constructor(private connectorService: WsConnectorService) {
+  constructor(private connectorService: WsConnectorService, public diagramComponent: DiagramComponent) {
     this.currentArgSystem = null;
   }
 
   ngOnInit() {
     this.connectorService.get<string[]>('systems').subscribe(data => this.argSystemIdList = data);
+    console.log(this.currentArgSystem);
   }
 
   retrieveArgumentationSystem(id: string): void {
+    console.log('id: ' + id);
     this.connectorService.get<IArgSystem>(id).subscribe(result => this.currentArgSystem = result);
   }
 
@@ -37,6 +40,12 @@ export class ConnectorComponent implements OnInit {
     console.log('Suspens...');
     console.log('Steps:');
     console.log(temp.businessSteps);
+
+    const deResult: ParseDiagramElementsResult = temp.getDiagramElements();
+
+    console.log("DERESULT");
+    console.log(deResult);
+    this.diagramComponent.showDiagram(deResult.listElements, deResult.businessSteps);
   }
 
 }
