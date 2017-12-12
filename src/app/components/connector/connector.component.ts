3e28241be26d3c15128c6e58a568d81b447fb5ec
Maxime Carlier <maxime.mca@gmail.com>
2017-12-12 18:31:19 +0100

    [FIX] Add missing import for rxjs

--- src/app/components/connector/connector.component.ts
+++ src/app/components/connector/connector.component.ts
@@ -7,6 +7,7 @@ import json = graphlib.json;
 import {StepToCreate} from '../../business/ArgSystem';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 import {Observable} from 'rxjs/Observable';
 import {NgModel} from '@angular/forms';
 
