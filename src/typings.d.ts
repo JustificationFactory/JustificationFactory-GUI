cf4cd7514306706cc4fd2fc299b2f5eff848aaa8
Maxime Carlier <maxime.mca@gmail.com>
2017-12-15 09:50:23 +0100

    [DEV] Remove unused typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,17 +3,3 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
-
-/* pdfkit module definition */
-declare class PDFDocument {
-  constructor(attributes: any);
-  public pipe : any;
-  public text(a: any, b: any, c: any);
-  public end();
-}
-
-declare class blobStream {
-
-}
-
-declare function SVGtoPDF (doc: any, svg: any, x: any, y: any, options?: any);
